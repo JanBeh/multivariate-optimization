89b35ec4df860fc43330309817ae0a36b60882f3
Jan Behrens <jbe@magnetkern.de>
2023-07-09 16:13:39 +0200

    Added Schwefel function

--- src/testfuncs.rs
+++ src/testfuncs.rs
@@ -1,5 +1,7 @@
 //! Test problems for optimization algorithms.
 
+use crate::optimize::SearchRange;
+
 /// Rastrigin function.
 ///
 /// Typically used from `-5.12..=5.12`. Minimum is at `0.0`.
@@ -30,4 +32,20 @@ pub fn rosenbrock(mut args: &[f64]) -> f64 {
         args = &args[2..];
     }
     sum
+}
+
+/// Parameter where Schwefel function is zero.
+pub const SCHWEFEL_TARGET: f64 = 420.968746;
+
+/// Search range for Schwefel function.
+pub const SCHWEFEL_RANGE: SearchRange = SearchRange::Finite {
+    low: -500.0,
+    high: 500.0,
+};
+
+/// Schwefel function.
+pub fn schwefel(args: &[f64]) -> f64 {
+    let dim = args.len() as f64;
+    let sum: f64 = args.iter().copied().map(|x| x * x.abs().sqrt().sin()).sum();
+    418.982887272433800 * dim - sum
 }
